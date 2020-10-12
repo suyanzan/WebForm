48e8af9151b54eeafa30243c0129a7cb1caefaa8
suyanzan <suyanzan@gmail.com>
2020-10-12 21:23:24 +0800

    Add files via upload

---
+++ Upload.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Upload.aspx.cs" Inherits="Webform_Test.Upload" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+       <h4>Select a file to upload:</h4>
+   
+       <asp:FileUpload id="FileUpload1"                 
+           runat="server">
+       </asp:FileUpload>
+            
+       <br /><br />
+       
+       <asp:Button id="UploadButton" 
+           Text="Upload file"
+           OnClick="UploadButton_Click"
+           runat="server">
+       </asp:Button>    
+       
+       <hr />
+       
+       <asp:Label id="UploadStatusLabel"
+           runat="server">
+       </asp:Label>        
+    </div>
+    </form>
+</body>
+</html>
---
+++ Upload.aspx.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Webform_Test
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void UploadButton_Click(object sender, EventArgs e)
+        {
+            // Specify the path on the server to
+            // save the uploaded file to.
+            String savePath = @"c:\temp\uploads\";
+            //FileUpload1.PostedFile.InputStream.Dispose();
+            File.Move(@"c:\temp\uploads\108164471_3233152086801988_6281965456476126455_o.jpg", @"c:\temp\uploads\girl.jpg");
+
+            // Before attempting to perform operations
+            // on the file, verify that the FileUpload 
+            // control contains a file.
+            if (FileUpload1.HasFile)
+            {
+                // Get the name of the file to upload.
+                String fileName = FileUpload1.FileName;
+                // Append the name of the file to upload to the path.
+                savePath += fileName;
+
+
+                // Call the SaveAs method to save the 
+                // uploaded file to the specified path.
+                // This example does not perform all
+                // the necessary error checking.               
+                // If a file with the same name
+                // already exists in the specified path,  
+                // the uploaded file overwrites it.
+                FileUpload1.SaveAs(savePath);
+
+                // Notify the user of the name of the file
+                // was saved under.
+                UploadStatusLabel.Text = "Your file was saved as " + fileName;
+            }
+            else
+            {
+                // Notify the user that a file was not uploaded.
+                UploadStatusLabel.Text = "You did not specify a file to upload.";
+            }
+        }
+    }
+}
